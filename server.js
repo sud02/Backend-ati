b8379812b6ecce3fc29e6ef8c85f36d22e07a5e8
sud02 <87568742+sud02@users.noreply.github.com>
2024-09-14 15:53:24 +0530

    Switch to bcryptjs and clean install dependencies

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const bcrypt = require('bcrypt');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
