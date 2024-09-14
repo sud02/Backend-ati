aa6b7b2a2e45c23aba4a5fa29422548a3726f8b7
sud02 <87568742+sud02@users.noreply.github.com>
2024-09-14 15:52:23 +0530

    Switch to bcryptjs to avoid Heroku compatibility issues

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
 const app = express();
