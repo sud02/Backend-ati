3ae31d7c98fa2cf4471610536fe65b39c70023dc
sud02 <87568742+sud02@users.noreply.github.com>
2024-09-14 15:30:02 +0530

    main

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 6000;
 
 app.use(cors());
 app.use(bodyParser.json());
